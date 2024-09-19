7dce54de42b1fed84771157061e92fc981779752
Adam Sparkes <adsparkes18@gmail.com>
2024-09-19 16:53:20 -0230

    Password customization

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
 
+
 const { program } = require('commander');
 
-// Function to generate the password with only lowercase letters
-function generatePassword(length) {
+// Function to generate the password
+function generatePassword(length, includeNumbers, includeUppercase, includeSymbols) {
     const lower = 'abcdefghijklmnopqrstuvwxyz';
-    
+    const numbers = '0123456789';
+    const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+
+    let characters = lower;
+    if (includeNumbers) characters += numbers;
+    if (includeUppercase) characters += uppercase;
+    if (includeSymbols) characters += symbols;
+
     let password = '';
     for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * lower.length);
-        password += lower[randomIndex];
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
     }
     return password;
 }
@@ -17,14 +26,22 @@ function generatePassword(length) {
 // CLI definition
 program
     .name('password-generator')
-    .description('CLI to generate simple passwords')
+    .description('CLI to generate secure passwords')
     .version('1.0.0')
     .option('-l, --length <number>', 'length of the password', '8')
+    .option('-n, --numbers', 'include numbers in the password')
+    .option('-u, --uppercase', 'include uppercase letters in the password')
+    .option('-s, --symbols', 'include symbols in the password')
     .parse(process.argv);
 
 // Parse flags and arguments
 const options = program.opts();
 const length = parseInt(options.length);
+
+// Convert flags to booleans
+const includeNumbers = !!options.numbers;
+const includeUppercase = !!options.uppercase;
+const includeSymbols = !!options.symbols;
 
 // Validate length input
 if (isNaN(length) || length < 1) {
@@ -33,8 +50,9 @@ if (isNaN(length) || length < 1) {
 }
 
 // Generate and display the password
-const generatedPassword = generatePassword(length);
+const generatedPassword = generatePassword(length, includeNumbers, includeUppercase, includeSymbols);
 console.log(`Generated Password: ${generatedPassword}`);
+
 
 
 
